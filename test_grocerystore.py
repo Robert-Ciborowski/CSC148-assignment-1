fe7a182b26689a4d5920f3cd16c8d84fc42625a5
ElijahBelov <44126195+ElijahBelov@users.noreply.github.com>
2019-10-13 22:08:08 -0400

    added grocery store tests

--- test_grocerystore.py
+++ test_grocerystore.py
@@ -25,7 +25,10 @@ GROCERY_STORE = '{\n  "regular_count": 3,\n   "express_count": 2,\n   ' \
                 '"self_serve_count": 4,\n   "line_capacity": 10\n}'
 
 GROCERY_STORE_2 = '{\n  "regular_count": 1,\n   "express_count": 1,\n   ' \
-                '"self_serve_count": 0,\n   "line_capacity": 1\n}'
+                  '"self_serve_count": 0,\n   "line_capacity": 1\n}'
+
+GROCERY_STORE_3 = '{\n  "regular_count": 1,\n   "express_count": 1,\n   ' \
+                  '"self_serve_count": 1,\n   "line_capacity": 2\n}'
 
 EXPRESS_LIMIT = 7
 
@@ -69,9 +72,63 @@ def test_enter_line() -> None:
 
 def test_line_is_ready() -> None:
     store = GroceryStore(GROCERY_STORE_2)
+    assert len(store._lines) == 2
     assert store.line_is_ready(0)
     assert store.line_is_ready(1)
-    pass
+    assert store.line_is_ready(0)
+    assert len(store._lines) == 2
+    item_list = [Item('bananas', 6)]
+    belinda = Customer('Belinda', item_list)
+    store.enter_line(belinda)
+    assert not store.line_is_ready(0)
+    assert store.line_is_ready(1)
+    assert len(store._lines) == 2
+
+
+def test_start_checkout() -> None:
+    store = GroceryStore(GROCERY_STORE_2)
+    assert store.start_checkout(0) == 0
+    assert len(store._lines[0]) == 0
+    item_list = [Item('bananas', 6)]
+    belinda = Customer('Belinda', item_list)
+    store.enter_line(belinda)
+    assert store.start_checkout(0) == 6
+    assert len(store._lines[0]) == 1
+
+
+def test_complete_checkout() -> None:
+    store = GroceryStore(GROCERY_STORE_2)
+    assert store.complete_checkout(0) == 0
+    item_list = [Item('bananas', 6)]
+    belinda = Customer('Belinda', item_list)
+    store.enter_line(belinda)
+    assert store.complete_checkout(0) == 1
+
+
+def test_close_line() -> None:
+    store = GroceryStore(GROCERY_STORE_3)
+    item_list = [Item('bananas', 6)]
+    item_list_2 = [Item('cherries', 5)]
+    item_list_3 = [Item('apples', 4)]
+    belinda = Customer('Belinda', item_list)
+    assert len(store.close_line(0)) == 0
+    store.enter_line(belinda)
+    assert len(store.close_line(1)) == 0
+    charlie = Customer('Charlie', item_list_2)
+    avery = Customer('Avery', item_list_3)
+    store.enter_line(charlie)
+    store.enter_line(avery)
+    assert len(store.close_line(2)) == 1
+
+
+def test_get_first_in_line() -> None:
+    store = GroceryStore(GROCERY_STORE_2)
+    assert store.get_first_in_line(0) is None
+    item_list = [Item('bananas', 6)]
+    belinda = Customer('Belinda', item_list)
+    store.enter_line(belinda)
+    assert store.get_first_in_line(0) == belinda
+
 
 if __name__ == '__main__':
     import pytest
